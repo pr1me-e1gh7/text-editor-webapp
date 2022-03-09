de118b4c612ffdfb820813950eb98d584bdce2d0
dcaro1996 <dcaro1996@yahoo.com>
2022-03-09 01:31:21 -0500

    fix attempt 2

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,35 +1,39 @@
 import { openDB } from 'idb';
 
+// Database generated
 const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+	openDB('jate', 1, {
+		upgrade(db) {
+			if (db.objectStoreNames.contains('jate')) {
+				console.log('jate database already exists');
+				return;
+			}
+			db.createObjectStore('jate', {
+				keyPath: 'id',
+				autoIncrement: true,
+			});
+			console.log('jate database created');
+		},
+	});
 
-// Add received content to DB
-export const putDb = async (content) => { 
-	const jateDb = await openDB('jate', 1);
+// iDB text added
+export const putDb = async (content) => {
+	const jateDB = await openDB('jate', 1);
+	const tx = jateDB.transaction('jate', 'readwrite');
+	const store = tx.objectStore('jate');
 	const request = store.put({ id: 1, value: content });
 	const result = await request;
-	const store = tx.objectStore('jate');
-	const tx = jateDb.transaction('jate', 'readwrite');
-	console.log('Added received content', result);
+	console.log('iDB - text added', result);
 };
-// Obtain content from DB
-export const getDb = async () => {
+
+// iDB text retrieved
+export const getDb = async (e) => {
 	const jateDb = await openDB('jate', 1);
+	const tx = jateDb.transaction('jate', 'readonly');
+	const store = tx.objectStore('jate');
 	const request = store.get(1);
 	const result = await request;
-	const store = tx.objectStore('jate');
-	const tx = jateDb.transaction('jate', 'readonly');
-
-	console.log('result.value', result);
+	console.log('iDB - text retrieved', result);
 	return result?.value;
 };
 
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,27 +1,26 @@
-const butInstall = document.getElementById('buttonInstall');
+const installBtn = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 
 // Event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    // Prompted Events storage
+	// Prompted Events storage
 	window.deferredPrompt = event;
-    butInstall.classList.toggle('hidden', false);
+	installBtn.classList.toggle('hidden', false);
 });
 
 // Install button event handler
-butInstall.addEventListener('click', async () => {
-    const prmptListener = window.deferredPrompt;
-
-    if (!prmptListener) {
-        return }
-    
-    prmptListener.prompt();
-    window.deferredPrompt = null;
-    butInstall.classList.toggle('hidden', true);
+installBtn.addEventListener('click', async () => {
+	const promptEvent = window.deferredPrompt;
+	if (!promptEvent) {
+		return;
+	}
+	promptEvent.prompt();
+	window.deferredPrompt = null;
+	installBtn.classList.toggle('hidden', true);
 });
 
 // Installed event
 window.addEventListener('appinstalled', (event) => {
-    window.deferredPrompt = null;
+	window.deferredPrompt = null;
 });
